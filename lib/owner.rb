a081e6e4772397e16fb1c9d5eb7e44a3efaa8b11
Zachary Lehmann <zlehmann@gmail.com>
2018-10-01 22:28:20 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,4 +43,14 @@ class Owner
     @pets[:fishes] << new_fish
   end
 
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[:cats] << new_cat
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
+  end
+
 end
